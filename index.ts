a01d773766608fc04c18581df0c8cbd824486830
kq5y <tkser.edu@gmail.com>
2024-11-15 23:24:25 +0900

    fix: style

--- index.ts
+++ index.ts
@@ -71,6 +71,8 @@ const rehypeMermaidCtm = (options: RehypeMermaidCtmConfig = {}) => {
         continue;
       }
       const svgElement = svgHtml.children[0] as Element;
+      const svgStyles = (svgElement.properties.style as string).split(";");
+      svgElement.properties.style = svgStyles[0] + ";";
       const nodeIndex = parent.children.indexOf(pre);
       parent.children[nodeIndex] = {
         type: "element",
